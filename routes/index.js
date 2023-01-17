e9411c7083618cfbdb6950ab78e767bb673d9148
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-17 13:47:02 -0500

    v1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("../db/db.json");
+const fs = require('fs')
+const path = require('path')
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 
+router.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
-app.get("/notes", (req, res) => {
-res.send('hello')
-})
+router.post("/notes", (req, res) => {
+  // incorperate body
+  const newNote = req.body;
+  // specifiy an id value
+  newNote.id = db.length;
+  // read previous notes, and add to them
+  readAndAppend(newNote, "./db/db.json");
+  res.json(newNote);
+});
 
-module.exports = router
+router.delete("/notes/:id", (req, res) => {
+  for (let i = 0; i < db.length; i++) {
+    let note = db[i];
+    if (note.id === req.params.id) {
+      db.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify(db)
+      );
+      break;
+    }
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const path = require("path");
-const api = require('./routes/index.js');
+const api = require('./routes');
 
-const PORT = process.env.PORT || 3001;
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(express.static("public"));
-
-// GET Route for feedback page
+// GET Route for notes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-// Wildcard route to direct users to a 404 page
+// GET route for index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
 });
